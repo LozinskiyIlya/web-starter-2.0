eed6f63673557e65ccb48436d739bc2c38e1ad8f
ilyalozinskiy <inbtrhope@gmail.com>
2024-03-30 21:26:47 +0100

    Swagger

---
+++ web/src/main/java/com/starter/web/configuration/ServerProperties.java
@@ -0,0 +1,21 @@
+package com.starter.web.configuration;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import java.net.URI;
+
+
+@Data
+@Validated
+@ConfigurationProperties("starter.server")
+public class ServerProperties {
+
+    @NotNull
+    private URI host;
+
+    @NotNull
+    private URI frontendHost;
+}
--- web/src/main/java/com/starter/web/configuration/WebAutoconfiguration.java
+++ web/src/main/java/com/starter/web/configuration/WebAutoconfiguration.java
@@ -1,9 +1,11 @@
 package com.starter.web.configuration;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
+@EnableConfigurationProperties(ServerProperties.class)
 @Import({com.starter.domain.configuration.DomainAutoconfiguration.class})
 public class WebAutoconfiguration {
 }
