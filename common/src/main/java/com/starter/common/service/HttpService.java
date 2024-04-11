6f29667cc72f03335379973f185ddf8326778ace
ilyalozinskiy <inbtrhope@gmail.com>
2024-04-11 21:21:52 +0200

    HttpService

---
+++ common/src/main/java/com/starter/common/service/HttpService.java
@@ -0,0 +1,70 @@
+package com.starter.common.service;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class HttpService {
+
+    private RestTemplate rest;
+    private HttpHeaders headers;
+
+    @PostConstruct
+    public void init() {
+        rest = new RestTemplate();
+        headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        headers.add("Accept", "*/*");
+    }
+
+    public String get(String uri) {
+        return this.getT(uri, String.class);
+    }
+
+    public <T> T getT(String uri, Class<T> responseClass) {
+        HttpEntity<String> requestEntity = new HttpEntity<>("", headers);
+        ResponseEntity<T> responseEntity = rest.exchange(uri, HttpMethod.GET, requestEntity, responseClass);
+        return responseEntity.getBody();
+    }
+
+    public <T> T getT(String uri, Class<T> responseClass, HttpHeaders customHeaders) {
+        HttpEntity<String> requestEntity = new HttpEntity<>("", customHeaders);
+        ResponseEntity<T> responseEntity = rest.exchange(uri, HttpMethod.GET, requestEntity, responseClass);
+        return responseEntity.getBody();
+    }
+
+    public <T> T getT(String uri, ParameterizedTypeReference<T> responseType) {
+        HttpEntity<String> requestEntity = new HttpEntity<>("", headers);
+        ResponseEntity<T> responseEntity = rest.exchange(uri, HttpMethod.GET, requestEntity, responseType);
+        return responseEntity.getBody();
+    }
+
+    public String post(String uri, String json) {
+        return this.postT(uri, json, String.class);
+    }
+
+    public <ReqT, ResT> ResT postT(String uri, ReqT body, Class<ResT> responseClass) {
+        HttpEntity<ReqT> requestEntity = new HttpEntity<>(body, headers);
+        ResponseEntity<ResT> responseEntity = rest.exchange(uri, HttpMethod.POST, requestEntity, responseClass);
+        return responseEntity.getBody();
+    }
+
+    public <ReqT, ResT> ResT postT(String uri, ReqT body, Class<ResT> responseClass, HttpHeaders customHeaders) {
+        HttpEntity<ReqT> requestEntity = new HttpEntity<>(body, customHeaders);
+        ResponseEntity<ResT> responseEntity = rest.exchange(uri, HttpMethod.POST, requestEntity, responseClass);
+        return responseEntity.getBody();
+    }
+
+    public <ReqT, ResT> ResT postT(String uri, ReqT body, ParameterizedTypeReference<ResT> responseType) {
+        HttpEntity<ReqT> requestEntity = new HttpEntity<>(body, headers);
+        ResponseEntity<ResT> responseEntity = rest.exchange(uri, HttpMethod.POST, requestEntity, responseType);
+        return responseEntity.getBody();
+    }
+}
+
