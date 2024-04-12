d509b5260c78acd491ee8deefe4952b662e60046
ilyalozinskiy <inbtrhope@gmail.com>
2024-04-12 11:33:53 +0200

    Parse open ai responses tests

---
+++ open-ai/src/test/java/com/starter/openai/service/AssistantResponseParserTest.java
@@ -0,0 +1,78 @@
+package com.starter.openai.service;
+
+import lombok.SneakyThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.StringUtils;
+import org.testcontainers.shaded.com.google.common.base.Charsets;
+import org.testcontainers.shaded.com.google.common.io.ByteSource;
+
+
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+class AssistantResponseParserTest {
+
+    @Autowired
+    private AssistantResponseParser responseParser;
+
+    @Test
+    @DisplayName("Parses full response")
+    void parsesFullResponse() {
+        var response = readResource("responses/open-ai/with_all_fields.txt");
+        var parsed = responseParser.parse(response);
+        assertTrue(StringUtils.hasText(parsed.getBuyer()));
+        assertTrue(StringUtils.hasText(parsed.getSeller()));
+        assertTrue(StringUtils.hasText(parsed.getAmount()));
+        assertTrue(StringUtils.hasText(parsed.getPurpose()));
+        assertTrue(StringUtils.hasText(parsed.getCurrency()));
+        assertTrue(StringUtils.hasText(parsed.getMentionedDate()));
+        assertTrue(StringUtils.hasText(parsed.getDateFormat()));
+        assertTrue(parsed.getTags().length > 0);
+    }
+
+    @Test
+    @DisplayName("Parses some fields response")
+    void parsesSomeFieldsResponse() {
+        var response = readResource("responses/open-ai/without_some_fields.txt");
+        var parsed = responseParser.parse(response);
+        assertTrue(StringUtils.hasText(parsed.getAmount()));
+        assertTrue(StringUtils.hasText(parsed.getPurpose()));
+        assertTrue(StringUtils.hasText(parsed.getCurrency()));
+        assertTrue(StringUtils.hasText(parsed.getMentionedDate()));
+        assertTrue(StringUtils.hasText(parsed.getDateFormat()));
+        assertTrue(parsed.getTags().length > 0);
+    }
+
+    @Test
+    @DisplayName("Parses empty response")
+    void parsesEmptyResponse() {
+        var response = readResource("responses/open-ai/without_all_fields.txt");
+        var parsed = responseParser.parse(response);
+        assertNotNull(parsed);
+    }
+
+
+    @SneakyThrows
+    public static String readResource(String filename) {
+        var template = new ClassPathResource(filename);
+        var text = "";
+        try (InputStream html = template.getInputStream()) {
+            ByteSource byteSource = new ByteSource() {
+                @Override
+                public InputStream openStream() {
+                    return html;
+                }
+            };
+            text = byteSource.asCharSource(Charsets.UTF_8).read();
+        }
+        return text;
+    }
+}
