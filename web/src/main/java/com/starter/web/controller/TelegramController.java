935d851fe3b0fa50cba7e5f2d490157d0639bd67
ilyalozinskiy <inbtrhope@gmail.com>
2024-04-26 14:44:56 +0200

    Telegram verifies init data

--- web/src/main/java/com/starter/web/controller/TelegramController.java
+++ web/src/main/java/com/starter/web/controller/TelegramController.java
@@ -10,16 +10,16 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
-import java.util.Base64;
-import java.util.Map;
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriUtils;
 
 @Slf4j
 @RestController
@@ -29,38 +29,55 @@ import org.springframework.web.bind.annotation.RestController;
 public class TelegramController {
 
     private final TelegramProperties telegramProperties;
-    private final ObjectMapper objectMapper;
 
     @SneakyThrows
     @PostMapping("/webapp/auth")
     public void verifyInitData(@RequestParam("initData") String initDataEncoded) {
-        // Decode initData
-        String initDataJson = new String(Base64.getDecoder().decode(initDataEncoded));
-        Map<String, String> initData = objectMapper.readValue(initDataJson, new TypeReference<>() {
-        });
-
-        // Extract hash and remove it from data
-        String hash = initData.get("hash");
-        initData.remove("hash");
-
-        // Create data check string
-        SortedMap<String, String> sortedData = new TreeMap<>();
-        initData.keySet().forEach(key -> sortedData.put(key, initData.get(key)));
-        String dataCheckString = sortedData.entrySet().stream()
-                .map(entry -> entry.getKey() + "=" + entry.getValue())
-                .reduce((a, b) -> a + "\n" + b)
-                .orElse("");
-
-        // Generate HMAC
-        SecretKeySpec secretKey = new SecretKeySpec(telegramProperties.getToken().getBytes(), "HmacSHA256");
-        Mac mac = Mac.getInstance("HmacSHA256");
-        mac.init(secretKey);
-        byte[] hmacBytes = mac.doFinal(dataCheckString.getBytes());
-        // Verify hash
-        String expectedHash = Hex.encodeHexString(hmacBytes);
-        if (!expectedHash.equals(hash)) {
-            throw new Exceptions.UnauthorizedException("");
+        if (!isValid(initDataEncoded)) {
+            throw new Exceptions.UnauthorizedException("Invalid init data");
         }
     }
 
+    private boolean isValid(String initDataEncoded) {
+        try {
+            String initDataDecoded = UriUtils.decode(initDataEncoded, StandardCharsets.UTF_8);
+
+            // Parse the query string
+            Map<String, String> params = Arrays.stream(initDataDecoded.split("&"))
+                    .map(param -> param.split("="))
+                    .collect(Collectors.toMap(p -> p[0], p -> UriUtils.decode(p[1], StandardCharsets.UTF_8)));
+
+            // Extract hash and remove it from params
+            String hash = params.remove("hash");
+
+            // Create data check string
+            String dataCheckString = params.entrySet().stream()
+                    .sorted(Map.Entry.comparingByKey())
+                    .map(entry -> entry.getKey() + "=" + entry.getValue())
+                    .collect(Collectors.joining("\n"));
+
+            // Generate the secret key
+            SecretKeySpec secretKey = new SecretKeySpec("WebAppData".getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+            Mac mac = Mac.getInstance("HmacSHA256");
+            mac.init(secretKey);
+            byte[] botTokenHmac = mac.doFinal(telegramProperties.getToken().getBytes(StandardCharsets.UTF_8));
+
+            // Compute HMAC using the bot token HMAC as the secret key
+            secretKey = new SecretKeySpec(botTokenHmac, "HmacSHA256");
+            mac.init(secretKey);
+            byte[] hmacBytes = mac.doFinal(dataCheckString.getBytes(StandardCharsets.UTF_8));
+            String expectedHash = bytesToHex(hmacBytes);
+            return hash.equals(expectedHash);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
 }
--- web/src/test/java/com/starter/web/controller/TelegramControllerIT.java
+++ web/src/test/java/com/starter/web/controller/TelegramControllerIT.java
@@ -11,13 +11,34 @@ class TelegramControllerIT extends AbstractSpringIntegrationTest {
 
     @SneakyThrows
     @Test
-    @DisplayName("verify init data")
-    void verifyInitData() {
+    @DisplayName("returns 4xx if empty")
+    void returns4xxIfEmpty() {
+        mockMvc.perform(postRequest("/webapp/auth")
+                        .param("initData", ""))
+                .andExpect(status().is4xxClientError());
+
+    }
+
+    @SneakyThrows
+    @Test
+    @DisplayName("returns 4xx if based on some foreign telegram token")
+    void returns4xxIfBasedOnForeignToken() {
+        final var initData = "query_id=AAEdElIZAAAAAB0SUhmMLJx-&user=%7B%22id%22%3A424808989%2C%22first_name%22%3A%22Ilya%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22ilialoz%22%2C%22language_code%22%3A%22en%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1714133271&hash=52785bed7aefd6c757762e1a08b4a213e7b359ddd458017d5a79ce0c857da042";
+        mockMvc.perform(postRequest("/webapp/auth")
+                        .param("initData", initData))
+                .andExpect(status().is4xxClientError());
+
+    }
+
+    @SneakyThrows
+    @Test
+    @DisplayName("verify init data signed with our telegram token")
+    void verifyInitDataSignedProperly() {
         // given
-        final var initDataSafe = "";
+        final var initDataSafe = "user=%7B%22id%22%3A424808989%2C%22first_name%22%3A%22Ilya%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22ilialoz%22%2C%22language_code%22%3A%22en%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=8865578296467838294&chat_type=sender&auth_date=1714134067&hash=f82a194643761180598c1d80512d07458114240983e0e3e7e2112cda86e7bc41";
         // when and then
         mockMvc.perform(postRequest("/webapp/auth")
-                .param("initData", initDataSafe))
+                        .param("initData", initDataSafe))
                 .andExpect(status().isOk());
 
     }
