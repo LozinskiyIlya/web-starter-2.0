f2ee81ddef562b30884b0f5fbd4cc1921ec7682b
ilyalozinskiy <inbtrhope@gmail.com>
2024-07-31 13:10:08 +0800

    GPT prompt trim

--- web/src/main/java/com/starter/web/service/bill/GroupService.java
+++ web/src/main/java/com/starter/web/service/bill/GroupService.java
@@ -14,7 +14,6 @@ import com.starter.web.mapper.BillMapper;
 import com.starter.web.mapper.GroupMapper;
 import com.starter.web.service.openai.OpenAiAssistant;
 import jakarta.transaction.Transactional;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
