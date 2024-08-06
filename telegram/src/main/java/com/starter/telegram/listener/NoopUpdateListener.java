5944d7ebc7766a7637ba812057c37a88bb1da5cf
ilyalozinskiy <inbtrhope@gmail.com>
2024-08-06 12:07:54 +0800

    caption for visionAPI

--- telegram/src/main/java/com/starter/telegram/listener/NoopUpdateListener.java
+++ telegram/src/main/java/com/starter/telegram/listener/NoopUpdateListener.java
@@ -4,6 +4,7 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.Update;
 import org.springframework.stereotype.Component;
 
+import static com.starter.telegram.service.TelegramBotService.latestKeyboard;
 import static com.starter.telegram.service.render.TelegramStaticRenderer.withLatestKeyboard;
 
 
@@ -12,6 +13,6 @@ public class NoopUpdateListener implements UpdateListener {
     @Override
     public void processUpdate(Update update, final TelegramBot bot) {
         bot.execute(withLatestKeyboard(update.message().chat().id(),
-                "Please use one of the available commands or buttons to interact with the bot."));
+                "Please use one of the available commands or buttons to interact with the bot.").replyMarkup(latestKeyboard()));
     }
 }
